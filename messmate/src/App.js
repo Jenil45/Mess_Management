09584aed2a4862ea2649120fbb58d946b24af0b0
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2023-03-22 12:55:44 +0530

    Add routing structure

--- messmate/src/App.js
+++ messmate/src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       Hello Quad Coders
     </div>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Layout/ >} >
+            <Route path='' element={<Home/>}> </Route>
+            <Route path='about' element={<About/>}> </Route>
+            <Route path='content' element={<Contact/>}> </Route>
+            <Route path='Allfaculty' element={<Allfaculty />}> </Route>
+            <Route path='exprfaculty' element={<Faculty10 />}> </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
