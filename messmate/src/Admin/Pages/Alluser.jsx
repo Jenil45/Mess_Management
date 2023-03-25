d00cbd280b6f0eada8a3ce0b9512a0a2788ff229
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2023-03-25 17:58:27 +0530

    add more functionality in searching

--- messmate/src/Admin/Pages/Alluser.jsx
+++ messmate/src/Admin/Pages/Alluser.jsx
@@ -39,50 +39,54 @@ function Alluser() {
     getData();
   }, []);
 
-  const content = users.filter((item)=>{
-  return (item.name.toLowerCase().includes(search))
-  }).slice(startingindex, startingindex + row).map((user) => {
-    return (
-      <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
-        <td className='w-4 p-4'></td>
-        <th
-          scope='row'
-          className='flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white'>
-          <div className=''>
-            <div className='text-black font-semibold'>{user.name}</div>
-          </div>
-        </th>
-        <td className='px-6 py-4'>{user.email}</td>
-        <td className='px-6 py-4'>
-          <div className='flex items-center'>{user.mobileno}</div>
-        </td>
-        <td className='px-6 py-4'>
-          <div className='flex items-center'>
-            {user.role === 1 ? (
-              <p className='bg-lime-300 rounded-md p-[2.5px] px-2  text-black text-sm'>Admin</p>
-            ) : (
-              <p className='px-2 text-black text-sm '>User</p>
-            )}
-          </div>
-        </td>
-        <td className='px-6 py-4'>
-          {/* <button>Edit </button> */}
-          <button
-            type='button'
-            class='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-3 py-[0.5rem] text-center mr-2 mb-2 '
-            onClick={() => console.log("Edit")}>
-            Edit
-          </button>
-          <button
-            type='button'
-            class='text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-3 py-[0.5rem] text-center mr-2 mb-2'
-            onClick={() => console.log("Delete")}>
-            Delete
-          </button>
-        </td>
-      </tr>
-    );
-  });
+  const content = users
+    .filter((item,i) => {
+      return (item.name.toLowerCase().includes(search) || item.email.toLowerCase().includes(search) || (item.role===0 ? 'user' : 'admin').includes(search)  ) ;
+
+    })
+    .slice(startingindex, startingindex + row)
+    .map((user) => {
+      return (
+        <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+          <td className='w-4 p-4'></td>
+          <th
+            scope='row'
+            className='flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white'>
+            <div className=''>
+              <div className='text-black font-semibold'>{user.name}</div>
+            </div>
+          </th>
+          <td className='px-6 py-4'>{user.email}</td>
+          <td className='px-6 py-4'>
+            <div className='flex items-center'>{user.mobileno}</div>
+          </td>
+          <td className='px-6 py-4'>
+            <div className='flex items-center'>
+              {user.role === 1 ? (
+                <p className='bg-lime-300 rounded-md p-[2.5px] px-2  text-black text-sm'>Admin</p>
+              ) : (
+                <p className='px-2 text-black text-sm '>User</p>
+              )}
+            </div>
+          </td>
+          <td className='px-6 py-4'>
+            {/* <button>Edit </button> */}
+            <button
+              type='button'
+              class='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-3 py-[0.5rem] text-center mr-2 mb-2 '
+              onClick={() => console.log("Edit")}>
+              Edit
+            </button>
+            <button
+              type='button'
+              class='text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-3 py-[0.5rem] text-center mr-2 mb-2'
+              onClick={() => console.log("Delete")}>
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
   // console.log(content)
   // const searchintable = (e) => {
   //   setSearch(e.target.value);
@@ -95,44 +99,46 @@ function Alluser() {
     .map((index, i) => {
       return (
         <button
-          className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+          className='relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex'
           onClick={() => {
             setPage(i);
             setstartingindex(i * row);
-          }}
-        >
+          }}>
           {i}
         </button>
       );
     });
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <div className="flex items-center justify-end pb-4 bg-white dark:bg-gray-900">
-        <label htmlFor="table-search" className="sr-only">
+    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+      <div className='flex items-center justify-end pb-4 bg-white dark:bg-gray-900'>
+        <label
+          htmlFor='table-search'
+          className='sr-only'>
           Search
         </label>
-        <div className="relative">
-          <div className="absolute inset-y-2 right-0 px-1  flex items-center pl-3 pointer-events-none">
+        <div className='relative'>
+          <div className='absolute inset-y-2 right-0 px-1  flex items-center pl-3 pointer-events-none'>
             <svg
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+              className='w-5 h-5 text-gray-500 dark:text-gray-400'
+              aria-hidden='true'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'>
               <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
+                fillRule='evenodd'
+                d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+                clipRule='evenodd'
               />
             </svg>
           </div>
           <input
-            type="text"
-            id="table-search-users"
-            className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search for users"
-            onChange={(e) => {  setSearch(e.target.value.toLowerCase())}}
+            type='text'
+            id='table-search-users'
+            className='block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            placeholder='Search for users'
+            onChange={(e) => {
+              setSearch(e.target.value.toLowerCase());
+            }}
             value={search}
           />
         </div>
@@ -141,23 +147,35 @@ function Alluser() {
       /*                                         table main part                                        */
       /* ---------------------------------------------------------------------------------------------- */}
 
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-white uppercase bg-dark dark:bg-gray-700 dark:text-gray-400">
+      <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+        <thead className='text-xs text-white uppercase bg-dark dark:bg-gray-700 dark:text-gray-400'>
           <tr>
-            <th scope="col" className="p-4"></th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope='col'
+              className='p-4'></th>
+            <th
+              scope='col'
+              className='px-6 py-3'>
               Name
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope='col'
+              className='px-6 py-3'>
               Email id
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope='col'
+              className='px-6 py-3'>
               Mobile no
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope='col'
+              className='px-6 py-3'>
               Role
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope='col'
+              className='px-6 py-3'>
               Operation
             </th>
           </tr>
@@ -186,26 +204,23 @@ function Alluser() {
           </nav> */}
         </tbody>
       </table>
-      <div className="flex mt-2 mb-2 ">
+      <div className='flex mt-2 mb-2 '>
         <nav
-          className="isolate m-auto min-h-[40px]  inline-flex mb-5 flex-space-x-px rounded-md shadow-sm"
-          aria-label="Pagination"
-        >
+          className='isolate m-auto min-h-[40px]  inline-flex mb-5 flex-space-x-px rounded-md shadow-sm'
+          aria-label='Pagination'>
           <button
             onClick={() => setPage(page - 1)}
-            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
-            <span className="sr-only">Previous</span>
+            className='relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'>
+            <span className='sr-only'>Previous</span>
             <svg
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              aria-hidden='true'>
               <path
-                fillRule="evenodd"
-                d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-                clipRule="evenodd"
+                fillRule='evenodd'
+                d='M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z'
+                clipRule='evenodd'
               />
             </svg>
           </button>
@@ -214,19 +229,17 @@ function Alluser() {
 
           <button
             onClick={() => setPage(page + 1)}
-            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
-            <span className="sr-only">Next</span>
+            className='relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'>
+            <span className='sr-only'>Next</span>
             <svg
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              aria-hidden='true'>
               <path
-                fillRule="evenodd"
-                d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                clipRule="evenodd"
+                fillRule='evenodd'
+                d='M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z'
+                clipRule='evenodd'
               />
             </svg>
           </button>
