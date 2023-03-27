7971b754015402f10362e050fd4ba47afea1875d
GunjanSurti <gunjansurti142@gmail.com>
2023-03-27 11:59:38 +0530

    update navBar & Home

--- messmate/src/Components/Home.jsx
+++ messmate/src/Components/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import homePng from "../Svg/Green Collage Daily Favorite Food Youtube Banner (3).png"
 const Home = () => {
-  return <div className="min-h-full">Home</div>;
-};
+  return (
+  <>
+  <div className="min-h-screen  ">
+    <img src={homePng} alt="" />
+  </div>
+
+  {/* <h1 className="">Eat Healthy,</h1>
+  <h1>Stay Healthy</h1> */}
+  {/* <div className="min-h-screen bg-lime-300">Home</div>; */}
+  </>
+)};
 
 export default Home;
