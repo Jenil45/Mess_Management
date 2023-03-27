f409fe6b1c0b4ecfa8ef76c7b061349b9e4789a0
GunjanSurti <gunjansurti142@gmail.com>
2023-03-27 13:20:03 +0530

    add Unauthorized Page

--- messmate/src/Components/Home.jsx
+++ messmate/src/Components/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import homePng from "../Svg/Green Collage Daily Favorite Food Youtube Banner (3).png"
+import homePng from "../Svg/Green Collage Daily Favorite Food Youtube Banner (3).png";
 const Home = () => {
   return (
-  <>
-  <div className="min-h-screen  ">
-    <img src={homePng} alt="" />
-  </div>
+    <>
+      <div className="min-h-screen max-w-full h-auto classes">
+        <img src={homePng} alt="" />
+      </div>
 
-  {/* <h1 className="">Eat Healthy,</h1>
+      {/* <h1 className="">Eat Healthy,</h1>
   <h1>Stay Healthy</h1> */}
-  {/* <div className="min-h-screen bg-lime-300">Home</div>; */}
-  </>
-)};
+      {/* <div className="min-h-screen bg-lime-300">Home</div>; */}
+    </>
+  );
+};
 
 export default Home;
