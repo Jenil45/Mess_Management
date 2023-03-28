8aa226710ac0f1129f6b5fe25c6dbb11b78f6d27
GunjanSurti <gunjansurti142@gmail.com>
2023-03-28 12:53:56 +0530

    .

--- messmate/src/Admin/Components/Sidebar.jsx
+++ messmate/src/Admin/Components/Sidebar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useLogout from "../../Api/Logout";
-
+/** Svg's */
+import hat from "../../Svg/hat.gif";
+import burger from "../../Svg/burger.gif";
+import logOut from "../../Svg/logout.gif";
+import confused from "../../Svg/confused.gif";
+import verified from "../../Svg/verified.gif";
+import hauntedHouse from "../../Svg/haunted-house.gif";
 function Sidebar() {
   const logout = useLogout();
   return (
     <div className="h-screen ">
-      <ul className="flex flex-col pl-0 mb-0 mt-2">
-        <li className="mt-5 w-full">
+      <ul className="flex flex-col pl-0 mb-0 mt-[3rem] gap-[1.4rem]">
+        <li className=" w-full">
           <Link
-            className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors"
+            className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors"
             to="/admin"
           >
-            <div className="bg-gradient-to-tl from-purple-700 to-pink-500 shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
-              {/* <img src={Dashboard} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+            
+              <img src={hauntedHouse} alt="sdfsd" className="h-[50px] w-[100px]" />
             </div>
             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               Home Page
@@ -21,26 +28,26 @@ function Sidebar() {
           </Link>
         </li>
 
-        <li className="mt-5 w-full">
+        <li className=" w-full">
           <Link
-            className="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
+            className="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
             to="/admin"
           >
-            <div className="shadow-soft-2xl bg-gradient-to-tl from-blue-500  to-green-400    mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-1.5">
-              {/* <img src={Payment} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+              <img src={verified} alt="sdfsd" className="h-[50px] w-[100px]" />
             </div>
             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               Daily Attendent
             </span>
           </Link>
         </li>
-        <li className="mt-5 w-full">
+        <li className=" w-full">
           <Link
-            className="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
+            className=" text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
             to="/admin/menu"
           >
-            <div className="shadow-soft-2xl bg-gradient-to-tl from-blue-500  to-green-400    mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-1.5">
-              {/* <img src={Payment} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+              <img src={burger} alt="sdfsd" className="h-[50px] w-[100px]" />
             </div>
             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               Menu Add
@@ -48,13 +55,13 @@ function Sidebar() {
           </Link>
         </li>
 
-        <li className="mt-5 w-full">
+        <li className=" w-full">
           <Link
-            className="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
+            className="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
             to="/admin/adduser"
           >
-            <div className="shadow-soft-2xl bg-gradient-to-tl from-blue-500  to-green-400    mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-1.5">
-              {/* <img src={subscription} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+              <img src={hat} alt="sdfsd" className="h-[50px] w-[100px]" />
             </div>
             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               Add user
@@ -62,19 +69,19 @@ function Sidebar() {
           </Link>
         </li>
 
-        <li className="w-full mt-5">
+        <li className="w-full ">
           <h6 className="pl-6 ml-2 text-lg font-bold leading-tight uppercase  opacity-100">
             Account pages
           </h6>
         </li>
 
-        <li className="mt-5 w-full">
+        <li className=" w-full">
           <Link
-            className="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
+            className="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
             to="/admin/alluser"
           >
-            <div className="shadow-soft-2xl bg-gradient-to-tl from-blue-500  to-green-400    mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-1.5">
-              {/* <img src={Profile} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+              <img src={confused} alt="sdfsd" className="h-[50px] w-[100px]" />
             </div>
             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               All User Detail
@@ -82,13 +89,14 @@ function Sidebar() {
           </Link>
         </li>
 
-        <li className="mt-5 w-full">
+        <li className="w-full">
           <button
             onClick={logout}
-            className="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
+            className="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
           >
-            <div className="shadow-soft-2xl bg-gradient-to-tl from-blue-500  to-green-400    mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-1.5">
-              {/* <img src={Profile} /> */}
+            <div className="shadow-soft-2xl    mr-2 flex h-[76px] w-[70px] items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-1.5">
+              
+              <img src={logOut} alt="Logout!" className="h-[50px] w-[100px]" />
             </div>
             <button className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">
               Log Out
