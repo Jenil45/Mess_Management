808c3b19052935237514323d6ef0614767e908b7
Jenil Thakor <jenil.rohit45@gmail.com>
2023-03-31 11:48:34 +0530

    attendance qr changes

--- messmate/src/User/Pages/EditProfile.jsx
+++ messmate/src/User/Pages/EditProfile.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import useAuth from "../../Auth/useAuth";
 import hackerPic from "../../Svg/hacker.png";
 
 const EditProfile = () => {
+  const { auth } = useAuth();
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-
           {/* ---------------------------------------------------------------------------------------------- */
           /*                                     Left part : information                                    */
           /* ---------------------------------------------------------------------------------------------- */}
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center ">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
+              {auth.userId}
               <br className="hidden lg:inline-block" />
-              readymade gluten
+              {auth.name}
             </h1>
             <p className="mb-8 leading-relaxed">
               Copper mug try-hard pitchfork pour-over freegan heirloom neutra
