5311e1e6778448c83eca65e6286bdd19ff586dd6
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2023-03-31 14:44:05 +0530

    just add

--- messmate/src/User/Pages/UserMenu.jsx
+++ messmate/src/User/Pages/UserMenu.jsx
@@ -3,13 +3,13 @@ import axios from "../../Api/axios";
 import Card from "./Card";
 import userMenu from "../../Svg/UserMenu.png";
 const UserMenu = () => {
-  const [day, setDay] = useState("Monday");
-  const [isMenuAvlbl, setIsMenuAvlbl] = useState(false);
+  const [ day, setDay ] = useState("Monday");
+  const [ isMenuAvlbl, setIsMenuAvlbl ] = useState(false);
 
-  const [menuB, setMenuB] = useState(null);
-  const [menuL, setMenuL] = useState(null);
-  const [menuD, setMenuD] = useState(null);
-  const [menuS, setMenuS] = useState(null);
+  const [ menuB, setMenuB ] = useState(null);
+  const [ menuL, setMenuL ] = useState(null);
+  const [ menuD, setMenuD ] = useState(null);
+  const [ menuS, setMenuS ] = useState(null);
 
   // fetch data for menu
   useEffect(() => {
@@ -21,10 +21,10 @@ const UserMenu = () => {
         });
 
         if (response.data.menu.length > 0) {
-          setMenuB(response.data.menu[0].menu_breakfast);
-          setMenuL(response.data.menu[0].menu_lunch);
-          setMenuD(response.data.menu[0].menu_dinner);
-          setMenuS(response.data.menu[0].special_menu);
+          setMenuB(response.data.menu[ 0 ].menu_breakfast);
+          setMenuL(response.data.menu[ 0 ].menu_lunch);
+          setMenuD(response.data.menu[ 0 ].menu_dinner);
+          setMenuS(response.data.menu[ 0 ].special_menu);
           setIsMenuAvlbl(true);
         } else {
           setMenuB(null);
@@ -45,7 +45,7 @@ const UserMenu = () => {
     };
 
     getData();
-  }, [day]);
+  }, [ day ]);
 
   return (
     <div className="flex flex-col min-h-[75vh] justify-between px-4 mt-2">
@@ -74,7 +74,7 @@ const UserMenu = () => {
                 value={day}
                 onChange={(e) => setDay(e.target.value)}
               >
-                {}
+                { }
                 <option value="Monday">Monday</option>
                 <option value="Tuesday">Tuesday</option>
                 <option value="Wednesday">WednesDay</option>
@@ -90,6 +90,13 @@ const UserMenu = () => {
 
           {isMenuAvlbl ? (
             <>
+              {menuS.length > 0 ? (
+                <div className="w-full mt-4 mx-2 rounded-2xl bg-[#00ffff] py-3 text-black font-semibold text-lg px-20 min-h-[30px]">
+                  Today Menu Special :     <span className="text-black text-xl"> {menuS.map((items) => { return items })}</span>
+                </div>
+              ) : (
+                ""
+              )}
               <div className="flex gap-[1rem] justify-around w-full mt-[2rem] ">
                 <Card name={"Breakfast"} menu={menuB} time={"8a.m.-10a.m."} />
                 <Card name={"Lunch"} menu={menuL} time={"12 p.m.- 2 p.m."} />
@@ -99,14 +106,7 @@ const UserMenu = () => {
                   time={"7:30 p.m.-9:30 p.m."}
                 />
               </div>
-              {menuS.length > 0 ? (
-                <div className="w-full mx-2 bg-[#00ffff] py-3 text-black font-semibold text-lg px-20 min-h-[30px]">
-                  {" "}
-                  Today Menu Special :{menuS} {console.log(menuS)}
-                </div>
-              ) : (
-                ""
-              )}
+
             </>
           ) : (
             <div className="flex  m-auto min-h-[40vh] mt-[1rem] justify-between flex-col items-center ">
