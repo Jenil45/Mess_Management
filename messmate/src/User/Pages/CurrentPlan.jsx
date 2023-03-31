f33b40e0e2f76f532e4229d19e77fce7c3547ba3
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2023-03-31 10:58:53 +0530

    add some css in user part

--- messmate/src/User/Pages/CurrentPlan.jsx
+++ messmate/src/User/Pages/CurrentPlan.jsx
@@ -40,9 +40,10 @@ const CurrentPlan = ({ planDetail, plan_id }) => {
 
   return (
     <section className="text-gray-600 body-font overflow-hidden">
+    <h1 className="text-center font-semibold text-4xl text-black"> Your Current Plan </h1>
       <div className="container px-5 py-16 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+        <div className="lg:w-4/5 mx-auto flex justify-center flex-wrap">
+          <div className="lg:w-1/2 w-full p-4 bg-gray-200 rounded-lg lg:pr-10 lg:py-6 mb-6 lg:mb-0">
             <h2 className="text-sm font-semibold pl-2 title-font text-red-600  tracking-widest">
               {planDetail.planId}
             </h2>
@@ -93,15 +94,16 @@ const CurrentPlan = ({ planDetail, plan_id }) => {
                 {dayjs(planDetail.end_date).diff(dayjs(), "day")}
               </span>
             </div>
-            <div className="flex mt-3">
-              <span className="title-font font-medium text-2xl text-gray-900">
+            <div className="flex mt-5 ">
+              <span className="title-font font-medium text-2xl pl-4 text-gray-900">
                 &#8377;{planDetail.fees}
               </span>
-              <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
+              <button className="flex ml-auto  text-black bg-blue-400 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded"
+              onClick = "">
+                Feedback
               </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
+              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ">
+                {/* <svg
                   fill="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -110,7 +112,7 @@ const CurrentPlan = ({ planDetail, plan_id }) => {
                   viewBox="0 0 24 24"
                 >
                   <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                </svg>
+                </svg> */}
               </button>
             </div>
           </div>
--- messmate/src/index.js
+++ messmate/src/index.js
@@ -6,11 +6,10 @@ import { AuthProvider } from './Auth/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
